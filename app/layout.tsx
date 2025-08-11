b8be4c9053306af6b6c7124a7a03c4f9ab0d37b1
Nguyễn Ngọc Minh Trí <bthong45@gmail.com>
2025-08-11 18:55:59 +0700

    dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+      <body className="bg-white text-zinc-900 dark:bg-zinc-900 dark:text-zinc-100 antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           {children}
           <Footer />
